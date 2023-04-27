abf828720894d5362383003ea80e8676911b124e
Radojica Popovc <108525632+radojicapopovic@users.noreply.github.com>
2023-04-27 02:13:58 +0200

    Task3-hook-useFormatted

---
+++ src/App.tsx
@@ -0,0 +1,42 @@
+// src/App.tsx
+import React, { useEffect } from "react";
+import users from "./users.json";
+import { useFormattedData } from "./hooks/useFormattedData";
+import "./App.css";
+
+type User = {
+  id: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+  gender: string;
+  zip: number;
+  birthdate: string;
+  city: string;
+};
+
+const App: React.FC = () => {
+  const { formatted, sortBy, filter, search } = useFormattedData(users);
+
+  useEffect(() => {
+    search("anderson");
+    filter(({ zip }) => zip > 486);
+    sortBy("firstName");
+  }, [search, filter, sortBy]);
+
+  return (
+    <div className="container">
+      <h1 className="title">User List</h1>
+      {formatted.map(({ id, firstName, lastName, birthdate }: User) => (
+        <div className="user" key={id}>
+          <h2>
+            {firstName} {lastName}
+          </h2>
+          <p>{birthdate}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default App;
---
+++ src/hooks/useFormattedData.ts
@@ -0,0 +1,57 @@
+// src/hooks/useFormattedData.ts
+import { useCallback, useState } from "react";
+
+type User = {
+  id: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+  gender: string;
+  zip: number;
+  birthdate: string;
+  city: string;
+  [key: string]: string | number;
+};
+
+export const useFormattedData = (data: User[]) => {
+  const [formatted, setFormatted] = useState<User[]>(data);
+
+  const sortBy = useCallback(
+    (property: keyof User | ((a: User, b: User) => number)) => {
+      let newFormatted = [...formatted];
+  
+      if (typeof property === "function") {
+        newFormatted.sort(property);
+      } else {
+        newFormatted.sort((a, b) => (a[property] > b[property] ? 1 : -1));
+      }
+  
+      setFormatted(newFormatted);
+    },
+    [formatted]
+  );
+  
+
+  const filter = useCallback(
+    (fn: (user: User) => boolean) => {
+      const newFormatted = formatted.filter(fn);
+      setFormatted(newFormatted);
+    },
+    [formatted]
+  );
+
+  const search = useCallback(
+    (searchTerm: string) => {
+      const newFormatted = data.filter((user) => {
+        return Object.values(user).some((value) =>
+          value.toString().toLowerCase().includes(searchTerm.toLowerCase())
+        );
+      });
+
+      setFormatted(newFormatted);
+    },
+    [data]
+  );
+
+  return { formatted, sortBy, filter, search };
+};
